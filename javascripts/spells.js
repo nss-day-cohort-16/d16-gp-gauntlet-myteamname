1d37f65be1d7520ae3f2bd6d278a6e8f1538cdda
Ben Marks <onodric@yahoo.com>
2016-11-10 20:17:37 -0600

    functionalitied spells.js

--- javascripts/spells.js
+++ javascripts/spells.js
@@ -1,14 +1,10 @@
 "use strict";
 
-/*
-  TODO: Modularize this code with IIFE or Browserify
- */
-let Spellbook = {};
-
-/*
-  Base spell function that defines name, damage, damage type
- */
-Spellbook.Spell = function() {
+let Spells = {};
+Spells.Spellbook = {};
+// Base spell function that defines name, damage, damage type
+ 
+Spells.Spellbook.Spell = function() {
   this.name = "";
   this.damage = 0;
 
@@ -19,19 +15,28 @@ Spellbook.Spell = function() {
     return this.name + " of " + this.type + " for " + this.damage + " damage!";
   };
 };
-/*
-  An elemental sphere that can be cast by a magical class
- */
 
-Spellbook.Sphere = function() {
-  console.log("this", this);
-  // this.name = "sphere";
+// An elemental sphere that can be cast by a magical class
+Spells.Spellbook.Sphere = function() {
+  console.log("Sphere this", this);
+  this.name = "sphere";
   this.damage = Math.floor(Math.random() * 10 + 10);
 
   let random = Math.round(Math.random() * (this.damageTypes.length - 1));
   this.type = this.damageTypes[random];
 };
-Spellbook.Sphere.prototype = new Spellbook.Spell();
-// console.log("Spellbook", Spellbook);
-console.log("Spellbook", Spellbook);
-module.exports = Spellbook;
+
+Spells.Spellbook.Sphere.prototype = new Spells.Spellbook.Spell();
+console.log("Spells.Spellbook", Spells.Spellbook);
+let myNewSpell1 = new Spells.Spellbook.Sphere();
+let myNewSpell2 = new Spells.Spellbook.Sphere();
+let myNewSpell3 = new Spells.Spellbook.Sphere();
+let myNewSpell4 = new Spells.Spellbook.Sphere();
+
+console.log("Spell1: ", myNewSpell1);
+console.log("Spell2: ", myNewSpell2);
+console.log("Spell3: ", myNewSpell3);
+console.log("Spell4: ", myNewSpell4);
+
+
+module.exports = Spells.Spellbook;
