a14054f40e78be59cd349bfe25e2f117ad56fbd3
Nathan T. Baker <nathantbaker@gmail.com>
2016-11-14 11:31:46 -0600

    adds animation to fight button

--- javascripts/battleground.js
+++ javascripts/battleground.js
@@ -1,7 +1,17 @@
 "use strict";
 
 let newStats = require('./fighter.js');
-let currentPlayer = [];
+
+let currentPlayer  = [],
+    battlegroundEl = $("#battleground"),
+    attackEl       = $("#attack"),
+    fightStatusEl  = $("#fight-status"),
+    pcOverviewEl   = $("#pc-overview"),
+    npcOverviewEl  = $("#npc-overview"),
+    pcImageEl      = $("#pc-image"),
+    npcImageEl     = $("#npc-image"),
+    pcHealth       = $("#pc-health"),
+    npcHealth      = $("#npc-health");
 
 // Draw random location and allow player to set new location
 $(".card__link").click(function(e) {
@@ -19,94 +29,9 @@ $(".card__link").click(function(e) {
 // Randomize location when use clicks for a new location
 function randomizeLocation() {
   let num = 1 + Math.floor(Math.random() * 17);
-  $( "#battleground" ).removeClass("b1 b2 b3 b4 b5 b6 b7 b8 b9 b10 b11 b12 b13 b14 b15 b16 b17");
-  $( "#battleground" ).addClass( "b" + num );
+  battlegroundEl.removeClass("b1 b2 b3 b4 b5 b6 b7 b8 b9 b10 b11 b12 b13 b14 b15 b16 b17");
+  battlegroundEl.addClass( "b" + num );
 }
-
-// Uncomment all this to test the fightFunction with two test objects
-
-// let fakePc = {
-//   class: {
-//     healthBonus: -5,
-//     intelligenceBonus: 40,
-//     name: "ninja",
-//     strengthBonus: -30
-//   },
-//   critChance: 0.05,
-//   critDamage: 20,
-//   health: 47,
-//   intelligence: 130,
-//   limbs: ["head", "neck", "arm", "leg", "torso"],
-//   playerName: "Bobo",
-//   skinColor: "grey",
-//   skinColors: ["grey"],
-//   species: {name: "human"},
-//   strength: 60,
-//   toString: function(){
-//     let output = [this.playerName,
-//       ": a ",
-//       this.skinColor,
-//       " skinned ",
-//       this.species.name,
-//       " ",
-//       this.class.name,
-//       " with ",
-//       this.health,
-//       " health. ",
-//       (this.class.magical) ? "Able to cast " : " Wielding a ",
-//       this.weapon.name,
-//       "!"
-//     ].join("");
-//     return output;
-//   },
-//   weapon: {
-//     damage: 14,
-//     name: "sphere",
-//     type: "water"
-//   }
-// };
-
-// let fakeNpc = {
-//   class: {
-//     healthBonus: 55,
-//     name: "thief",
-//     strengthBonus: 30
-//   },
-//   critChance: 0.05,
-//   critDamage: 20,
-//   health: 143,
-//   intelligence: 90,
-//   limbs: ["head", "neck", "arm", "leg", "torso"],
-//   playerName: "Ned Nederlander",
-//   skinColor: "grey",
-//   skinColors: ["grey"],
-//   species: {name: "human"},
-//   strength: 120,
-//   toString: function(){
-//     let output = [this.playerName,
-//       ": a ",
-//       this.skinColor,
-//       " skinned ",
-//       this.species.name,
-//       " ",
-//       this.class.name,
-//       " with ",
-//       this.health,
-//       " health. ",
-//       (this.class.magical) ? "Able to cast " : " Wielding a ",
-//       this.weapon.name,
-//       "!"
-//     ].join("");
-//     return output;
-//   },
-//   weapon: {
-//     damage: 18,
-//     name: "war axe",
-//     hands: 2
-//   }
-// };
-
-// let testArray = [fakePc, fakeNpc];
 
 let fightFuntion = function fight(array) {
 
@@ -116,31 +41,34 @@ let fightFuntion = function fight(array) {
   // Print two players based on current stats
   let string1 = array[0].toString();
   let string2 = array[1].toString();
-  $("#pc-overview").html(string1);
-  $("#pc-health").html(`Health: ${array[0].health}`);
-  $("#pc-image").html(`<img src="images/fighters/${array[0].class.name}-stand.gif">`);
-  $("#npc-overview").html(string2);
-  $("#npc-health").html(`Health: ${array[1].health}`);
-  $("#npc-image").html(`<img src="images/fighters/${array[1].class.name}-stand.gif">`);
+  pcOverviewEl.html(string1);
+  pcHealth.html(`Health: ${array[0].health}`);
+  pcImageEl.html(`<img src="images/fighters/${array[0].class.name}-stand.gif">`);
+  npcOverviewEl.html(string2);
+  npcHealth.html(`Health: ${array[1].health}`);
+  npcImageEl.html(`<img src="images/fighters/${array[1].class.name}-stand.gif">`);
 
   // If someone has zero health, show the results of the fight
   if ( array[0].health === 0 || array[1].health === 0 ) {
     if ( array[0].health === 0) {
-      $("#fight-status").html("<div class='title'>You Lost!</div>");
+      fightStatusEl.html("<div class='title'>You Lost!</div>");
     } else if (array[1].health === 0) {
-      $("#fight-status").html("<div class='title'>You Win!</div>");
+      fightStatusEl.html("<div class='title'>You Win!</div>");
     }
   }
 };
 
-// Fighter.js is called each time the attack button is pressed
-$("#attack").click( () => {
-  let newArray = newStats(currentPlayer[0], currentPlayer[1]);
-  fightFuntion(newArray);
+// Animate fight everytime Attack button clickd
+attackEl.click( () => {
+  pcImageEl.html(`<img src="images/fighters/${currentPlayer[0].class.name}-fight.gif">`);
+  npcImageEl.html(`<img src="images/fighters/${currentPlayer[1].class.name}-fight.gif">`);
+  window.setTimeout(runNewData, 1200);
 });
 
-// Run this function to test the fightFunction. This should be called in app.js and passed an array of the two players as the argument. argument is [pc, npc]
-
-// fightFuntion(testArray);
+// Function to run after animation runs
+function runNewData() {
+  let newArray = newStats(currentPlayer[0], currentPlayer[1]);
+  fightFuntion(newArray);
+}
 
 module.exports = fightFuntion;
